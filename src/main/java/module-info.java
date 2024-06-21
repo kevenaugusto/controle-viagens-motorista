06d7028e7b09ae33a7cd32c6fdfb1b85a373ee4c
Keven Augusto <kevenbezerra@gmail.com>
2024-06-21 00:34:53 -0400

    build: add lombok and hibernate dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module br.ufms.apsoo.controlador {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
+    requires jakarta.persistence;
 
 
     opens br.ufms.apsoo.controlador to javafx.fxml;
