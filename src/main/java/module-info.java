67e2ad6f08a4e604b05a938827a09694d3b3898c
Keven Augusto <kevenbezerra@gmail.com>
2024-06-25 02:32:15 -0400

    feat: connect to mysql database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,11 @@ module br.ufms.apsoo.controlador {
     requires javafx.fxml;
     requires static lombok;
     requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
 
     opens br.ufms.apsoo.controlador to javafx.fxml;
     exports br.ufms.apsoo.controlador;
+
+    opens br.ufms.apsoo.controlador.model to org.hibernate.orm.core;
 }
