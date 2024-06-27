2c54e6b2f353b3f12700100bd6683ec23265b99e
Keven Augusto <kevenbezerra@gmail.com>
2024-06-26 21:44:51 -0400

    feat: initialize driver's list view

--- src/main/java/br/ufms/apsoo/controlador/controller/MainFrameController.java
+++ src/main/java/br/ufms/apsoo/controlador/controller/MainFrameController.java
@@ -1,14 +1,29 @@
 package br.ufms.apsoo.controlador.controller;
 
+import br.ufms.apsoo.controlador.model.Motorista;
+import br.ufms.apsoo.controlador.model.Veiculo;
+import br.ufms.apsoo.controlador.model.Viagem;
+import br.ufms.apsoo.controlador.service.MotoristaService;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainFrameController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView<Motorista> listViewMotoristas;
+
+    private final MotoristaService motoristaService;
+
+    public MainFrameController() {
+        this.motoristaService = new MotoristaService();
+    }
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        motoristaService.loadMotoristaListView(listViewMotoristas);
     }
 }
